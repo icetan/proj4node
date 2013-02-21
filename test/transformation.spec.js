0a71123df8779d191c2ab2b41a34122952ab901e
Christopher Fredén <christopher.freden@kartena.se>
2013-02-21 18:44:26 +0100

    Added test NPM test script

---
+++ test/transformation.spec.js
@@ -0,0 +1,7 @@
+var proj4node = require('../lib/proj4node');
+
+describe("transformation", function() {
+  it("transform from lon/lat to projected x/y", function() {
+    expect(true).toBe(true);
+  });
+});
