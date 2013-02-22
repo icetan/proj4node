ec7ae27c438019c86f38c4ed678334aba33c0a14
Christopher <c.freden@gmail.com>
2013-02-22 02:05:09 +0100

    Added convenience transform on Proj to readme

--- test/transformation.spec.js
+++ test/transformation.spec.js
@@ -10,13 +10,13 @@ describe("transformation", function() {
     var proj = new Proj(testData.defs[data.code]),
         xyResult, llResult;
     it("of "+data.code+" should transform lon/lat to projected x/y", function() {
-      xyResult = proj.transform(WGS84, new Point(data.ll)); //{x:data.ll[0], y:data.ll[1]});
+      xyResult = proj.transform(WGS84, {x:data.ll[0], y:data.ll[1]});
       expect(xyResult.x).toBeCloseTo(data.xy[0], 2);
       expect(xyResult.y).toBeCloseTo(data.xy[1], 2);
     });
 
     it("of "+data.code+" should transform map x/y to lon/lat", function() {
-      llResult = WGS84.transform(proj, new Point(data.xy)); //{x:data.xy[0], y:data.xy[1]});
+      llResult = WGS84.transform(proj, {x:data.xy[0], y:data.xy[1]});
       expect(llResult.x).toBeCloseTo(data.ll[0], 6);
       expect(llResult.y).toBeCloseTo(data.ll[1], 6);
     });
